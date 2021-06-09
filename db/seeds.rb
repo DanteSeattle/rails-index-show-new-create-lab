8bb6da2a408be78a8df3185a5a0e04c7bd7678e6
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-06-08 17:28:15 -0700

    completed MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.destroy_all
+
+5.times do
+    Coupon.create(coupon_code: "Codes!", store: "Target")
+end
+
+puts "Done!"
